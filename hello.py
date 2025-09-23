bfe658128ff6931c3fe8ba6379338c20bcb6b598
MV2 <maheshchary660@gmail.com>
2025-09-23 19:11:57 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,21 @@
+print("Lakshith")
+print("welcome to python classes")
+print(1+2)
+print(4-3)
+print(3*4)
+print(16/4)
+print(6%4)
+print(6//4)
+print(2**3)
+print(3**3)
+x2=10
+print(x2)
+y=x2+2
+print(y)
+z=y+x2
+print(z)
+# not=10
+not2=10
+my_height=173
+h=173
+todays_max_temp=33
