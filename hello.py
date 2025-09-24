c4a29d50a131cb766bd9112ccb28ffcd06983b44
MV2 <maheshchary660@gmail.com>
2025-09-24 20:05:31 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -19,3 +19,116 @@ not2=10
 my_height=173
 h=173
 todays_max_temp=33
+
+
+
+# Assignment Operators
+
+# =
+# +=
+# -=
+# *=
+# /=
+# %=
+# //=
+# **=
+
+cs=10
+cs=cs+4
+cs+=4
+cs+=1
+cs+=5
+
+b=15
+b-=4
+print(b)
+
+b*=2
+print(b)
+
+b%=3
+print(b)
+
+f=1.4
+print(type(f))
+print(type(b))
+
+
+
+# Data types
+# int
+# float
+# bool
+# str - used to represent text
+
+k=True
+l=False
+print(type(k))
+is_raining=False
+
+name="Pramanicus Academy"
+student1="Lakshith"
+student2='Charan'
+print(type(name))
+print(name)
+print(student1)
+print(student2)
+
+temp="98.4"
+#f=1.8*c+32
+temp=float(temp) #98.4
+c=(temp-32)/1.8
+print(c)
+
+# Type conversion
+# Changing one data type to another
+# int() - str 
+# float() - str #int,float
+# str() - int,float
+
+wtemp="29"
+# wtemp=int(wtemp)
+wtemp=float(wtemp)
+print(wtemp)
+
+f=20.5
+print(type(f))
+s=str(f)
+print(type(s))
+print(f,s)
+
+# Comparison Operators
+# ==, !=, >, <, >=, <=
+print(f==10.6)
+print(10!=10)
+print(5>10)
+print(5<10)
+print(6>=10)
+print(7<=10)
+
+print()
+print()
+# Logical Operators
+# and, or, not
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print()
+print(True or True)
+print(True or False)
+print(False or True)    
+print(False or False)
+
+print()
+print(not True)
+print(not False)
+
+# Types of operators based on number of operands
+# Unary - 1 operand
+# Binary - 2 operands
+
+# 5+4
+# operand     operator     operand
+#    5            +           3
